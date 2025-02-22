dc14bcaf50226af90efe85bda3da57f7e8166b92
Tuvshee555 <tuvshee894@gmail.com>
2025-02-22 17:52:50 +0800

    fff

--- src/app/(food-menu)/_features/page.tsx
+++ src/app/(food-menu)/_features/page.tsx
@@ -1,20 +1,47 @@
-"use client";
-import axios from "axios";
-import { useEffect } from "react";
+// "use client";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { addCatagory } from "../_components/AddCatagory";
 
-export default function Home() {
-  const getData = async () => {
-    const response = await axios.get(`http://localhost:4000/users`);
-    console.log(response);
-  };
+// export default function Home() {
+//   const [catagory, setCatagory] = useState([]);
+//   const getData = async () => {
+//     const response = await axios.get(`http://localhost:4000/users`);
+//     console.log(response);
+//   };
+  
 
-  useEffect(() => {
-    getData();
-  }, []);
-  return (
-    <>
-      <div className="h-screen w-screen bg-[white] text-[black]">admin</div>
-      <div></div>
-    </>
-  );
-}
+//   useEffect(() => {
+//     getData();
+//   }, []);
+//   return (
+//     <>
+//       <div className="h-screen w-screen bg-[white] text-[black] flex">
+//         <div className="h-screen w-[205px] p-[36px] align-start flex flex col border border-2-red">
+//           <div>
+//             <div className="flex mb-[24px]">
+//               <img
+//                 src="./order.png"
+//                 className="w-[36px] h-[30px] flex flex-col-center"
+//               />
+//               <div>
+//                 <div className="flex align-center text-[18px] font-600 font inter">
+//                   NomNom
+//                 </div>
+//                 <div className="flex align-center text-[12px] font-600 font inter text-[#71717a]">
+//                   Swift delivery
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="flex flex-col gap-[8px]">
+//               <div className="text-[black]">Food menu</div>
+//               <div className="text-[black]">Orders</div>
+//               <div className="text-[black]">Settings</div>
+//             </div>
+//           </div>
+//         </div>
+//         <addCatagory />
+//       </div>
+//     </>
+//   );
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import AddCatagory from "@/components/AddCatagory";
+
+type Datas = {
+    username: string
+}
 
 export default function Home() {
+  const [catagory, setCatagory] = useState([]);
+  const [catagoriesName, setCatagoriesName] = useState<Datas[]>([])
+  const [error, setError] = useState(null);
   const getData = async () => {
-    const response = await axios.get(`http://localhost:4000/users`);
-    console.log(response);
+    try {
+      const response = await axios.get(`http://localhost:4000/users`);
+      console.log(response);
+      setCatagoriesName(response.data);
+    } catch (error: any) {
+      setError(error.message);
+    }
   };
 
   useEffect(() => {
@@ -13,8 +26,35 @@ export default function Home() {
   }, []);
   return (
     <>
-      <div className="h-screen w-screen bg-[white] text-[black]">admin</div>
-      <div></div>
+      <div className="h-screen w-screen bg-[white] text-[black] flex">
+        <div className="h-screen w-[205px] p-[36px] align-start flex flex col border border-2-red">
+          <div>
+            <div className="flex mb-[24px]">
+              <img
+                src="./order.png"
+                className="w-[36px] h-[30px] flex flex-col-center"
+              />
+              <div>
+                <div className="flex align-center text-[18px] font-600 font inter">
+                  NomNom
+                </div>
+                <div className="flex align-center text-[12px] font-600 font inter text-[#71717a]">
+                  Swift delivery
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-[8px]">
+              <div className="text-[black]">Food menu</div>
+              <div className="text-[black]">Orders</div>
+              <div className="text-[black]">Settings</div>
+            </div>
+          </div>
+        </div>
+        <AddCatagory />
+        {catagoriesName?.map((name, index) => (
+          <h1 key={index}>{name.username}</h1>
+        ))}
+      </div>
     </>
   );
 }
