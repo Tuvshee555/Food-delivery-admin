f1285504c52061a1336dabbe44c967b7e01a054e
Түвшинсайхан <tuvshee894@gmail.com>
2025-09-07 19:03:39 +0800

    closing login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { Suspense } from "react";
-import { AuthProvider } from "@/provider/AuthProvider";
+// import { AuthProvider } from "@/provider/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,12 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <Suspense>
-            {children}
-            <Toaster />
-          </Suspense>
-        </AuthProvider>
+        {/* <AuthProvider> */}
+        <Suspense>
+          {children}
+          <Toaster />
+        </Suspense>
+        {/* </AuthProvider> */}
       </body>
     </html>
   );
