423855ff56bc26a25209317ffdd7565920542a86
Түвшинсайхан <tuvshee894@gmail.com>
2026-01-10 10:27:57 +0800

    just adding some types in revenuestartscards

--- src/app/[locale]/(revenue)/components/components/RevenueStatsCards.tsx
+++ src/app/[locale]/(revenue)/components/components/RevenueStatsCards.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardContent } from "@/components/ui/card";
-import { RevenueData } from "../RevenueStatsCards";
+import { RevenueData } from "../../RevenueDashboard";
 
 type Props = {
   t: (key: string) => string;
